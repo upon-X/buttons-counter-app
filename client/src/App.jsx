48be34f02a8154e85e3a62ed02d5f7cb2182c05b
Miche <valemiche003@gmail.com>
2023-11-30 22:39:21 -0300

    changing Axios.defaults.baseUrl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import Button from './components/Boton';
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:3000/buttons/";
+import backURL from './backURL'
+axios.defaults.baseURL = backURL;
 
 function App() {
   return (
