70ebee06275dee227ae6f215447f73f8858e3a9e
upon-X <valemiche003@gmail.com>
2024-05-08 01:15:52 -0300

    idk to put here

--- server/index.js
+++ server/index.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://buttons-counter-app.vercel.app",
+    credentials: true, // If your front end needs to pass credentials
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use((req, res, next) => {
   // Set CORS headers
   res.header("Access-Control-Allow-Origin", "*"); // Adjust this to match the domain in production
