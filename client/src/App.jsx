fbbbc9469c6cc0569cadb2d73f0972c3b437807f
upon-X <valemiche003@gmail.com>
2024-05-08 00:13:21 -0300

    updated and fix back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Boton';
-import axios from 'axios';
-axios.defaults.baseURL = "https://buttons-counter-app-production.up.railway.app/buttons/";
+import React from "react";
+import "./App.css";
+import Button from "./components/Boton";
+import axios from "axios";
+axios.defaults.baseURL = "https://back-btncounterapp.vercel.app/buttons/";
 
 function App() {
-  return (
-    <Button />
-  )
+  return <Button />;
 }
 
-export default App
+export default App;
